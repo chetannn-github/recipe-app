d3e75726147cf35afee5b757455ab8078f5414f9
Chetan Singh Rajawat <=chetan.rajawat25@gmail.com>
2024-06-10 09:36:35 +0530

    completed responsiveness for smart phones

--- src/Components/Recomended.jsx
+++ src/Components/Recomended.jsx
@@ -1,10 +1,24 @@
-import React from 'react'
+import React, { useRef, useState } from 'react'
 import Cards from './Cards'
 import "../Stylesheets/Recomended.css"
+import useReciepe from '../utils/customHooks/useReciepe';
+import { Search } from 'lucide-react';
 
 const Recomended = () => {
-  return (
+  let searchquery = useRef(null);
+  const [query, setQuery] = useState(null);
+  const handleSearch = (e) => {
+    if (e.key === 'Enter') {
+      setQuery(searchquery.current.value);
+    }
+  };
+  useReciepe(query);
+   return (
     <div id='recomended'>
+      <div id="search">
+     <Search/><input ref={searchquery} type="text" onKeyDown={handleSearch}  placeholder='Mummy! Aaj kya ban rha hh?'></input>
+     </div>
+      
       <h1>Recomended  Recipes </h1>
       <p>Popular choices</p>
       <Cards/>
@@ -12,4 +26,4 @@ const Recomended = () => {
   )
 }
 
-export default Recomended
+export default Recomended;
--- src/utils/customHooks/useReciepe.js
+++ src/utils/customHooks/useReciepe.js
@@ -9,17 +9,18 @@ const APP_KEY = import.meta.env.VITE_APP_KEY;
 
 
 const useReciepe = (searchQuery="baati") =>{
-    useEffect(()=>fetchReciepe(),[]);
+   
+  
     let dispatch = useDispatch();
+    console.log(searchQuery)
 
-
-    const fetchReciepe = async () =>{
+    const fetchReciepe = async (searchQuery) =>{
         let res =  await fetch(`https://api.edamam.com/api/recipes/v2/?app_id=${APP_ID}&app_key=${APP_KEY}&q=${searchQuery}&type=public`);
         let data = await res.json();
         console.log(data.hits);
         dispatch(addReciepe(data.hits));
     }
-    
+     useEffect(()=>{fetchReciepe(searchQuery)},[searchQuery]);
 }
 
 export default useReciepe;
